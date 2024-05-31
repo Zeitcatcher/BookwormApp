0a4d636d899f369464999855296e83ef683c5542
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-05-30 16:40:23 -1000

    Initial Commit

---
+++ BookwormApp/BookwormAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormAppApp.swift
+//  BookwormApp
+//
+//  Created by Arseniy Oksenoyt on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
